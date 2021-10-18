3c1a44733334bf907416e4ed38e44034825fa8ce
PEDRO CAVALCANTI <pedro.vieira02@hotmail.com>
2021-10-18 00:34:25 -0300

    primeira implementacao da webApi

---
+++ Dio.Series.Web/SerieModel.cs
@@ -0,0 +1,36 @@
+﻿using DIO.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dio.Series.Web
+{
+    public class SerieModel
+    {
+        public int Id { get; set; }
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        public bool Excluido { get; set; }
+
+        //construtor
+        public SerieModel() { }
+
+        public SerieModel(Serie serie)
+        {
+            Id = serie.Id;
+            Genero = serie.retornaGenero();
+            Titulo = serie.retornaTitulo();
+            Descricao = serie.retornaDescricao();          
+            Ano = serie.retornaAno();
+            Excluido = serie.retornaExcluido();
+        }
+
+        public Serie ToSerie()
+        {
+            return new Serie(Id, Genero, Titulo, Descricao, Ano);
+        }
+    }
+}
